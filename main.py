be97840bb51c75e797babfe700a624cf271e09b6
Derek Wang <wxrderek2024@gmail.com>
2025-08-11 15:10:38 -0400

    feat - added Path class and handling for cubic Bezier segments

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from utils import write_svg, build_svg_doc
 from features.random_circles import random_circles, random_circles_on_grid, random_walk_circles
+from svg_shapes.path import Path
 
 # set viewbox width and height
 VIEWBOX_WIDTH, VIEWBOX_HEIGHT = 1000, 1000
 
 def main():
 
+    '''
     # test for random circles on grid
     grid_circles = random_circles_on_grid(grid_size=100, view_width=VIEWBOX_WIDTH, view_height=VIEWBOX_HEIGHT)
     svg_grid_circles = build_svg_doc(grid_circles)
@@ -15,6 +17,16 @@ def main():
     rw_circles = random_walk_circles(step_size=15, n=10000, view_width=VIEWBOX_WIDTH, view_height=VIEWBOX_HEIGHT)
     svg_rw_circles = build_svg_doc(rw_circles)
     write_svg(svg_rw_circles, filename='random_walk_circles.svg')
+    '''
+
+    # test Path class
+    path_test = Path(500, 500, fill='none', stroke='#0077cc', stroke_width=10)
+    path_test.add_cubic_bezier(700, 300, 900, 700, 100, 900)
+    path_test.add_cubic_bezier(100, 700, 300, 300, 900, 100)
+    path_test.add_cubic_bezier(700, 300, 900, 700, 100, 900)
+    path_test.add_cubic_bezier(100, 700, 300, 300, 500, 500)
+    svg_path_test = build_svg_doc(path_test.to_svg())
+    write_svg(svg_path_test, filename='path_test.svg')
 
 if __name__ == '__main__':
     main()
---
+++ svg_shapes/path.py
@@ -0,0 +1,40 @@
+# svg_shapes/path.py
+
+class Path:
+    def __init__(
+        self, start_x: float, start_y: float,
+        fill: str = "none", fill_opacity: float = 1.0,
+        stroke: str = "#000000", stroke_width: float = 1.0,
+        class_: str = None
+    ):
+        '''Create svg path element with specific start point only'''
+        self.start = (start_x, start_y)
+        self.fill = fill
+        self.fill_opacity = fill_opacity
+        self.stroke = stroke
+        self.stroke_width = stroke_width
+        self.class_ = class_
+
+        # initialize 'd' attribute at specified start point
+        self.d = f"M {start_x} {start_y}"
+
+
+    def add_cubic_bezier(self, c1x: float, c1y: float, c2x: float, c2y: float, x: float, y: float):
+        '''Append a cubic Bezier segment to the path'''
+        self.d += f" C {c1x} {c1y}, {c2x} {c2y}, {x} {y}"
+
+
+    def to_svg(self) -> str:
+        '''Returns svg path element as a string'''
+        attrs = [
+            f'd="{self.d}"',
+            f'fill="{self.fill}"',
+            f'fill-opacity="{self.fill_opacity}"',
+            f'stroke="{self.stroke}"',
+            f'stroke-width="{self.stroke_width}"'
+        ]
+
+        if self.class_:
+            attrs.append(f'class="{self.class_}"')
+
+        return f'<path {" ".join(attrs)} />'
